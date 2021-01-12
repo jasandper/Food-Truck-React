7e6d82f98c666263d01593ba4947bb4742a694f8
Jason Perez <jasandper@icloud.com>
2021-01-12 14:53:03 -0600

    setup backend server side

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from "./components/Navbar/Navbar";
 import './App.css';
+import Signinform from "./components/Signinform"
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Signinform />
     </div>
   );
 }
