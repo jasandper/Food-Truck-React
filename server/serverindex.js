8b3de58bc792f1d7ec3e34d35fad7cb7389eeb01
Jason Perez <jasandper@icloud.com>
2021-01-12 15:46:11 -0600

    backend: connect database and server

--- server/serverindex.js
+++ server/serverindex.js
@@ -1,11 +1,43 @@
-const express = require("express");
+const express = require('express');
+const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+// Database
+const db = require('./db');
+
+
+// Test DB
+db.authenticate()
+  .then(() => console.log('Database connected...'))
+  .catch(err => console.log('Error: ' + err))
+
 const app = express();
-const cors = require("cors");
 
-//middleware
-app.use(cors());
-app.use(express.json());
+// Handlebars
+app.engine('handlebars', exphbs({ defaultLayout: 'main',
+runtimeOptions: {
+    allowProtoPropertiesByDefault: true,
+    allowProtoMethodsByDefault: true,
+},
+}));
+app.set('view engine', 'handlebars');
 
-app.listen(4000, () => {
-    console.log("server has started on port 4000...")
-});
+// Body Parser
+app.use(bodyParser.urlencoded({ extended: false}));
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Index route
+app.get('/', (req, res) => res.render('index', { layout: 'landing'}));
+
+// Truck routes
+app.use('/trucks', require('./routes/trucks'));
+
+const PORT = process.env.PORT || 2000;
+
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
+
+
+
