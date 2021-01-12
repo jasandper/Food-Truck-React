cad078719753b6790b5c6a7f7f8df0f21750bd13
Jason Perez <jasandper@icloud.com>
2021-01-12 10:46:06 -0600

    initialize and setup navbar

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,23 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'All Trucks',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Add Truck',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Sign In',
+        url: '#',
+        cName: 'nav-links-mobile'
+    }
+
+]
